6ecd50879c9c4e172f47195b7ed4611e893f2cfd
Amir Hossein Montazerolghaem <amir.montazer@gmail.com>
2025-02-03 19:52:16 +0330

    Add search API test for varzesh3

---
+++ AutomationTest.Integration/Features/Varzesh3.feature
@@ -0,0 +1,10 @@
+﻿Feature: Varzesh3
+
+A short summary of the feature
+
+@tag1
+Scenario: Search for "تیم ملی ایران"
+	Given the API endpoint is "https://www.varzesh3.com/search"
+	When I search for "تیم ملی ایران"
+    Then the response should contain "تیم ملی ایران"
+    And the status code should be 200
---
+++ AutomationTest.Integration/StepDefinitions/Varzesh3StepDefinitions.cs
@@ -0,0 +1,37 @@
+﻿namespace AutomationTest.Integration.StepDefinitions;
+
+[Binding]
+public class Varzesh3StepDefinitions
+{
+    private HttpClient _client;
+    private HttpResponseMessage _response;
+    private string _baseUrl;
+
+    [Given(@"the API endpoint is ""([^""]*)""")]
+    public void GivenTheAPIEndpointIs(string baseUrl)
+    {
+        _baseUrl = baseUrl;
+        _client = new HttpClient();
+    }
+
+    [When(@"I search for ""([^""]*)""")]
+    public async Task WhenISearchFor(string query)
+    {
+        var requestUri = $"{_baseUrl}?q={query}";
+        _response = await _client.GetAsync(requestUri);
+    }
+
+    [Then(@"the response should contain ""([^""]*)""")]
+    public async Task ThenTheResponseShouldContain(string expectedText)
+    {
+        var content = await _response.Content.ReadAsStringAsync();
+        Assert.Contains(expectedText, content);
+    }
+
+    [Then(@"the status code should be (.*)")]
+    public void ThenTheStatusCodeShouldBe(int expectedStatusCode)
+    {
+        var statusCode = (int)_response.StatusCode;
+        Assert.Equal(expectedStatusCode, (int)_response.StatusCode);
+    }
+}
